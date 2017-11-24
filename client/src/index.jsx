8dc5ef031e97322ae1480b0311318895d0c36e03
johadi10 <jim.hadi10@gmail.com>
2017-11-25 00:54:04 +0100

    chore(setUp): add relevant folders

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+      <h2>The React App works!</h2>
+    </div>
+);
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
