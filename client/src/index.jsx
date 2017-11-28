48ccce8bebb979a606975f370977afd75e28e273
johadi10 <jim.hadi10@gmail.com>
2017-11-28 20:15:57 +0100

    chore(setUp): add static files and other tools required for front end

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,16 @@
+// This is the App entry file
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store/store';
+import App from './components/App';
 
-const App = () => (
-    <div>
-      <h2>The React App works!</h2>
-    </div>
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#app')
 );
-
-ReactDOM.render(<App/>, document.querySelector('#app'));
